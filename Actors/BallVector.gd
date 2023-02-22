206d45b3659b9fce63dfccfde32b162df39719bb
Christopher Angelo <angelo@angeloanan.xyz>
2023-02-22 07:54:32 +0100

    refactor: unshadow

--- Actors/BallVector.gd
+++ Actors/BallVector.gd
@@ -5,6 +5,6 @@ var force := Vector2.ZERO
 func _draw() -> void:
   draw_line(Vector2(0, 0), force, Color.blue, 8)
 
-func update_force(force: Vector2) -> void:
-  self.force = force
+func update_force(new_force: Vector2) -> void:
+  self.force = new_force
   update()
