d8d7fe422d3f722feecdd6e0614a5388b43f9c35
Christopher Angelo <angelo@angeloanan.xyz>
2023-02-23 00:45:52 +0100

    feat: varying ball vector variation

--- Actors/BallVector.gd
+++ Actors/BallVector.gd
@@ -3,7 +3,16 @@ extends Node2D
 var force := Vector2.ZERO
 
 func _draw() -> void:
-  draw_line(Vector2.ZERO, force, Color.red)
+  var line_color := Color.black
+  var line_thickness := 2
+  
+  if BallData.power_ball:
+    line_thickness = 4
+
+  if BallData.water_ball:
+    line_color = Color.blue
+
+  draw_line(Vector2.ZERO, force, line_color, line_thickness, true)
   $RayCast2D.cast_to = force
   # TODO: Recursive function that draws a line for each force's bounce normal
 
