7ba583c1e5f25a3e1512f9229585c35034b1844f
Christopher Angelo <angelo@angeloanan.xyz>
2023-02-23 00:40:50 +0100

    feat: powerup ui

--- Autoload/BallData.gd
+++ Autoload/BallData.gd
@@ -2,6 +2,8 @@ extends Node
 
 signal stroke_total_update
 signal stroke_count_update
+signal power_ball_update
+signal water_ball_update
 
 # The number of strokes the player has taken in the whole game
 var stroke_total := 1 setget set_stroke_total
@@ -10,8 +12,8 @@ var stroke_total := 1 setget set_stroke_total
 var stroke_count := 1 setget set_stroke_count
 
 # Power up switches
-var water_ball := false
-var power_ball := false
+var water_ball := false setget set_water_ball
+var power_ball := false setget set_power_ball
 
 func set_stroke_total(score: int) -> void:
   stroke_total = score
@@ -20,6 +22,16 @@ func set_stroke_total(score: int) -> void:
 func set_stroke_count(score: int) -> void:
   stroke_count = score
   emit_signal("stroke_count_update")
+
+func set_water_ball(state: bool) -> void:
+  water_ball = state
+  emit_signal("water_ball_update")
+
+func set_power_ball(state: bool) -> void:
+  power_ball = state
+  emit_signal("power_ball_update")
+
+# ----
 
 func reset() -> void:
   stroke_total = 1
--- UserInterface/UserInterface.gd
+++ UserInterface/UserInterface.gd
@@ -7,9 +7,13 @@ onready var stroke_total: Label = get_node("StrokeTotal")
 func _ready() -> void:
   BallData.connect("stroke_count_update", self, "update_interface")
   BallData.connect("stroke_total_update", self, "update_interface")
+  BallData.connect("power_ball_update", self, "update_interface")
+  BallData.connect("water_ball_update", self, "update_interface")
   update_interface()
 
 func update_interface() -> void:
   stroke_number.text = "Stroke: %s" % BallData.stroke_count
   stroke_total.text = "Total: %s" % BallData.stroke_total
+  $PowerUpIcons/Powerball.visible = BallData.power_ball
+  $PowerUpIcons/Waterball.visible = BallData.water_ball
 
