6e706d9d60153445c53a54e7c26a039226215aae
Christopher Angelo <angelo@angeloanan.xyz>
2023-02-23 22:12:43 +0100

    feat: powerup sfx

--- Objects/PowerUps/PowerUp.gd
+++ Objects/PowerUps/PowerUp.gd
@@ -3,8 +3,17 @@ extends Area2D
 # Check out Autoload/BallData; PowerUpType must be the variable name
 export var PowerUpType := "power_ball"
 onready var anim_player: AnimationPlayer = $AnimationPlayer
+onready var sfx: AudioStreamPlayer = $PowerUpAudio
 
 func _on_body_entered(body: Node) -> void:
-  BallData[PowerUpType] = true
+  
+  sfx.play()
   anim_player.stop()
+  self.visible = false
+  
+  BallData[PowerUpType] = true
+
+  yield(sfx, "finished")
+  
   self.queue_free()
+  
