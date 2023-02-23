ae5065de8e1fbea80a9615fc3e25ea1e252ca6e6
Christopher Angelo <angelo@angeloanan.xyz>
2023-02-23 13:04:51 +0100

    feat: reset balldata on start game

--- Screen/MainScreen.gd
+++ Screen/MainScreen.gd
@@ -7,4 +7,5 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
   if event.is_action_pressed("coin_add"):
+    BallData.reset()
     get_tree().change_scene_to(load("res://Levels/Level1.tscn"))
